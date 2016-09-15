b672e0c030cdb4d283c8fefd28a5d95fc635afb3
Afoninsky <vkfont@gmail.com>
2016-09-15 23:39:51 +0300

    runnner progress

--- docker-runner/build.sh
+++ docker-runner/build.sh
@@ -6,4 +6,5 @@ IMAGE=afoninsky/microdeploy
 
 #docker login
 docker build -t ${IMAGE}:latest --no-cache=true --pull=true --rm=true . | tee build.log || exit 1
+# docker build -t ${IMAGE}:latest . | tee build.log || exit 1
 #docker push ${IMAGE}:latest
