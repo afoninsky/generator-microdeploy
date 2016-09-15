7084da32859069bab89465cf71b67a072c6e22e7
Andrey <vkfont@gmail.com>
2016-09-15 20:39:13 +0300

    .

--- docker-runner/build.sh
+++ docker-runner/build.sh
@@ -3,9 +3,7 @@
 set -o pipefail
 
 IMAGE=afoninsky/microdeploy
-NODE_VERSION=6.5.0
-IMAGE_VERSION=latest
 
 #docker login
-docker build -t ${IMAGE}:latest --build-arg NODE_VERSION=${NODE_VERSION} --no-cache=true . | tee build.log || exit 1
-docker push ${IMAGE}:${IMAGE_VERSION}
+docker build -t ${IMAGE}:latest --no-cache=true --pull=true --rm=true . | tee build.log || exit 1
+#docker push ${IMAGE}:latest
