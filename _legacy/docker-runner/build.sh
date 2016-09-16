5bbda762901e741410d395ab03f23d5802b51bc3
Andrey <vkfont@gmail.com>
2016-09-16 18:20:56 +0300

    .

--- _legacy/docker-runner/build.sh
+++ _legacy/docker-runner/build.sh
@@ -2,9 +2,31 @@
 
 set -o pipefail
 
-IMAGE=afoninsky/microdeploy
+IMAGE=afoninsky/microdeploy:ndgk
+EMAIL=edissons-admin@spair-api.iam.gserviceaccount.com
+PROJECT_ID=spair-api
+ZONE=us-central1-f
+CLUSTER=edissons-cluster
 
 #docker login
-docker build -t ${IMAGE}:latest --no-cache=true --pull=true --rm=true . | tee build.log || exit 1
-# docker build -t ${IMAGE}:latest . | tee build.log || exit 1
+docker build -t ${IMAGE}:latest \
+	--no-cache=true --pull=true --rm=true \
+	--build-arg EMAIL=$EMAIL --build-arg PROJECT_ID=$PROJECT_ID --build-arg ZONE=$ZONE --build-arg CLUSTER=$CLUSTER \
+	. | tee build.log || exit 1
+#docker push ${IMAGE}:latest
+#!/bin/bash
+
+set -o pipefail
+
+IMAGE=afoninsky/microdeploy:ndgk
+EMAIL=edissons-admin@spair-api.iam.gserviceaccount.com
+PROJECT_ID=spair-api
+ZONE=us-central1-f
+CLUSTER=edissons-cluster
+
+#docker login
+docker build -t ${IMAGE}:latest \
+	--no-cache=true --pull=true --rm=true \
+	--build-arg EMAIL=$EMAIL --build-arg PROJECT_ID=$PROJECT_ID --build-arg ZONE=$ZONE --build-arg CLUSTER=$CLUSTER \
+	. | tee build.log || exit 1
 #docker push ${IMAGE}:latest
